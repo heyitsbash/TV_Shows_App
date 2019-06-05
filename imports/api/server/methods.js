9d88ae7c084224a83c505d1ae009cf36ce16e7d1
Eugene Rodionov <eugene.rodionov.firefly@gmail.com>
2019-06-05 17:15:14 +0300

    chore: add new method for tmdb Api

--- imports/api/server/methods.js
+++ imports/api/server/methods.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 import tvShows from '../collections/tvShows.js';
 
 const traktAPIKey = Meteor.settings.private.traktApiKey;
+const tmdbAPIKey = Meteor.settings.private.tmdbApiKey;
 
 Meteor.methods({
   tasksInsert: (text) => {
@@ -31,6 +32,32 @@ Meteor.methods({
         'Content-Type': 'application/json',
         'trakt-api-version': 2,
         'trakt-api-key': traktAPIKey,
+      },
+    })
+      .then((data) => {
+        const response = data.data;
+        const successResponse = {
+          type: 'success',
+          data: response,
+        };
+        return successResponse;
+      })
+      .catch((error) => {
+        const errorResponse = {
+          type: 'error',
+          data: error,
+        };
+        // return errorResponse;
+        throw new Error(errorResponse);
+      });
+  },
+  callTmdbAPI: (tvShowId) => {
+    const url = `https://api.themoviedb.org/3/tv/${tvShowId}?api_key=${tmdbAPIKey}&language=en-US&append_to_response=images,videos&include_image_language=ru,null`;
+    return axios({
+      method: 'get',
+      url,
+      headers: {
+        'Content-Type': 'application/json',
       },
     })
       .then((data) => {
