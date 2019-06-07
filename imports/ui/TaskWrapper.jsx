86fedd273a506c0618822ef3c0321631384178e6
Eugene Rodionov <eugene.rodionov.firefly@gmail.com>
2019-06-07 16:32:39 +0300

    chore: now using react-virtualized package

--- imports/ui/TaskWrapper.jsx
+++ imports/ui/TaskWrapper.jsx
@@ -1,83 +1,65 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { withTracker } from 'meteor/react-meteor-data';
 import { Meteor } from 'meteor/meteor';
 import { useDispatch } from 'react-redux';
-import tasksInsert from '../redux/actions/insertTask.js';
+import {
+  Column,
+  Table,
+  AutoSizer,
+  WindowScroller,
+  InfiniteLoader,
+} from 'react-virtualized';
 import sortAction from '../redux/actions/sortAction.js';
 import callTrakt from '../redux/actions/tracktCall.js';
 import loadPaginationAction from '../redux/actions/loadPaginationAction.js';
 import tvShows from '../api/collections/tvShows.js';
-import Task from './Task.jsx';
+// import 'react-virtualized/styles.css';
 
 const TaskWrapper = ({ shows, incompleteCount }) => {
-  const [inputVal, setInputVal] = useState('');
-  const [hideCompleted, setHideCompleted] = useState(false);
-
   const dispatch = useDispatch();
-  const insertTask = (text) => dispatch(tasksInsert(text));
   const sortingAction = () => dispatch(sortAction());
   const callingTrakt = (url) => dispatch(callTrakt(url));
   const paginationLoad = (pages) => dispatch(loadPaginationAction(pages));
 
-  const handleScroll = () => {
-    const scrolledBy = window.innerHeight + window.pageYOffset;
-    const bottomOfThePage = document.body.scrollHeight;
-    // const calculateScrollBorder = bottomOfThePage - bottomOfThePage * 0.1;
-    if (scrolledBy >= bottomOfThePage) {
-      paginationLoad(50);
-    }
-  };
-
-  useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
+  const lust = shows.map((show, index) => {
+    return {
+      index: index + 1,
+      name: show.title,
+      description: show.watched.allTime,
+      airDate: show.additionalInfo.first_air_date,
+      img: `https://image.tmdb.org/t/p/w780${show.image}`,
     };
   });
-
-  const renderTasks = () => {
-    let filteredTasks = shows;
-    if (hideCompleted) {
-      filteredTasks = filteredTasks.filter((show) => !show.checked);
-    }
-    return filteredTasks.map((show) => <Task key={show._id} show={show} />);
-  };
 
   const changeListOrder = () => {
     sortingAction();
   };
 
-  // const page = 0;
   const testAPIcall = () => {
-    // callingTrakt('https://api.trakt.tv/shows/trending?page=1&limit=10000');
     callingTrakt('https://api.trakt.tv/shows/464?extended=full');
-    // page += 1;
-    // console.log(page);
-    // const url = `https://api.trakt.tv/shows/watched/all?page=${page}&limit=1`;
-    // console.log(url);
-    // Meteor.call('callTraktAPI', url);
-    // callingTrakt(url);
   };
 
   const loadMorePages = () => {
-    paginationLoad(50);
+    paginationLoad(20);
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const text = inputVal.trim();
-    if (text === '') {
-      return;
-    }
-    insertTask(text);
-    setInputVal('');
+  // eslint-disable-next-line react/prop-types
+  const renderImageCell = ({ cellData }) => {
+    return <img src={cellData} alt="" height="250px" />;
   };
+
+  const renderIndexCell = ({ rowIndex }) => {
+    const index = rowIndex + 1;
+    return index;
+  };
+
+  renderImageCell.displayName = 'renderImageCell';
 
   return (
     <div className="container">
       <header>
-        <h1>Todo List - {incompleteCount}</h1>
+        <h1>TV Shows - {incompleteCount}</h1>
         <button type="button" onClick={changeListOrder}>
           Order
         </button>
@@ -87,26 +69,67 @@ const TaskWrapper = ({ shows, incompleteCount }) => {
         <button type="button" onClick={loadMorePages}>
           Load More
         </button>
-        <label htmlFor="hideCompletedInputId" className="hide-completed">
-          <input
-            id="hideCompletedInputId"
-            type="checkbox"
-            readOnly
-            checked={hideCompleted}
-            onClick={() => setHideCompleted(!hideCompleted)}
-          />
-          Hide completed shows
-        </label>
-        <form onSubmit={handleSubmit} className="new-task">
-          <input
-            type="text"
-            value={inputVal}
-            onChange={(e) => setInputVal(e.target.value)}
-            placeholder="type your task"
-          />
-        </form>
       </header>
-      <ul>{renderTasks()}</ul>
+      <div>
+        <InfiniteLoader
+          isRowLoaded={({ index }) => !!lust[index]}
+          loadMoreRows={loadMorePages}
+          rowCount={100000000}
+        >
+          {({ onRowsRendered, registerChild }) => (
+            <WindowScroller>
+              {({ height, scrollTop }) => (
+                <AutoSizer disableHeight>
+                  {({ width }) => (
+                    <Table
+                      ref={registerChild}
+                      onRowsRendered={onRowsRendered}
+                      autoHeight
+                      headerStyle={{
+                        display: 'flex',
+                      }}
+                      rowStyle={{
+                        display: 'flex',
+                      }}
+                      width={width}
+                      scrollTop={scrollTop}
+                      height={height}
+                      headerHeight={30}
+                      rowHeight={250}
+                      rowCount={lust.length}
+                      rowGetter={({ index }) => lust[index]}
+                    >
+                      <Column
+                        label="№"
+                        dataKey="index"
+                        cellRenderer={renderIndexCell}
+                        width={width * 0.05}
+                      />
+                      <Column
+                        cellRenderer={renderImageCell}
+                        label="img"
+                        dataKey="img"
+                        width={width * 0.25}
+                      />
+                      <Column label="Name" dataKey="name" width={width * 0.3} />
+                      <Column
+                        label="Views Count"
+                        dataKey="description"
+                        width={width * 0.2}
+                      />
+                      <Column
+                        label="First Air Date"
+                        dataKey="airDate"
+                        width={width * 0.2}
+                      />
+                    </Table>
+                  )}
+                </AutoSizer>
+              )}
+            </WindowScroller>
+          )}
+        </InfiniteLoader>
+      </div>
     </div>
   );
 };
