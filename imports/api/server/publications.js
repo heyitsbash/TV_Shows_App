b847d72dfd2510d66351347a5ef9010a6d45aca7
Eugene Rodionov <eugene.rodionov.firefly@gmail.com>
2019-06-18 21:19:44 +0300

    chore: heroku should start working any second

--- imports/api/server/publications.js
+++ imports/api/server/publications.js
@@ -1,11 +1,11 @@
 import { Meteor } from 'meteor/meteor';
 import TvShows from '../collections/TvShows.js';
-import { writeToSettings, getSettingsFile } from './writeToSettings.js';
+// import { writeToSettings, getSettingsFile } from './writeToSettings.js';
 
-const allShows = JSON.stringify(TvShows.find({}).count());
-const settingsFile = getSettingsFile();
-settingsFile.public.totalShowCount = allShows;
-writeToSettings(settingsFile);
+// const allShows = JSON.stringify(TvShows.find({}).count());
+// const settingsFile = getSettingsFile();
+// settingsFile.public.totalShowCount = allShows;
+// writeToSettings(settingsFile);
 
 Meteor.publish('TvShows', (payload) => {
   const { sortMethod, limit, filter } = payload;
--- imports/api/server/writeToSettings.js
+++ imports/api/server/writeToSettings.js
@@ -1,15 +1,15 @@
-import FS from 'fs';
+// import FS from 'fs';
 
-const writeToSettings = (settingsFile) => {
-  const updateSettingsFile = JSON.stringify(settingsFile, null, 2);
-  FS.writeFile('../../../settings.json', updateSettingsFile, () => {
-    // console.log(`Updated settings file..`);
-  });
-};
+// const writeToSettings = (settingsFile) => {
+//   const updateSettingsFile = JSON.stringify(settingsFile, null, 2);
+//   FS.writeFile('../../../settings.json', updateSettingsFile, () => {
+//     // console.log(`Updated settings file..`);
+//   });
+// };
 
-const getSettingsFile = () => {
-  const settingsFile = JSON.parse(FS.readFileSync('../../../settings.json'));
-  return settingsFile;
-};
+// const getSettingsFile = () => {
+//   const settingsFile = JSON.parse(FS.readFileSync('../../../settings.json'));
+//   return settingsFile;
+// };
 
-export { writeToSettings, getSettingsFile };
+// export { writeToSettings, getSettingsFile };
