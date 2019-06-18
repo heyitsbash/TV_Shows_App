2c12f3b99768f1bd3af01645971eac625241be18
Eugene Rodionov <eugene.rodionov.firefly@gmail.com>
2019-06-18 21:14:24 +0300

    chore: heroku pls stop

--- imports/api/server/writeToSettings.js
+++ imports/api/server/writeToSettings.js
@@ -2,15 +2,13 @@ import FS from 'fs';
 
 const writeToSettings = (settingsFile) => {
   const updateSettingsFile = JSON.stringify(settingsFile, null, 2);
-  FS.writeFile('../../../../../settings.json', updateSettingsFile, () => {
+  FS.writeFile('../../../settings.json', updateSettingsFile, () => {
     // console.log(`Updated settings file..`);
   });
 };
 
 const getSettingsFile = () => {
-  const settingsFile = JSON.parse(
-    FS.readFileSync('../../../../../settings.json')
-  );
+  const settingsFile = JSON.parse(FS.readFileSync('../../../settings.json'));
   return settingsFile;
 };
 
